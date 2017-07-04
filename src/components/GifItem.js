e4f67ed985ecb1cb082563a66b5ccf1df46f3020
Lee Haugen <haugenlee@leegenes.local>
2017-07-04 14:49:35 -0400

    add giflist and gifitem compenents with fake data

---
+++ src/components/GifItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const GifItem = (image) => {
+  return (
+    <li>
+      <img src={image.gif.url} />
+    </li>
+  )
+};
+export default GifItem;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/SearchBar'
+import GifList from './components/GifList';
+import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      gifs: [
+        {
+          id: 1,
+          url: 'http://fakeimg.pl/300'
+        },
+        {
+          id: 2,
+          url: 'http://fakeimg.pl/300/'
+        },
+        {
+          id: 3,
+          url: 'http://fakeimg.pl/300/'
+        }
+      ]
+    }
+  }
+
   handleTermChange(term) {
     console.log(term);
   }
+
     render() {
         return (
             <div className="greeting">
                 <SearchBar onTermChange={this.handleTermChange} />
+                <GifList gifs={this.state.gifs} />
             </div>
         );
     }
