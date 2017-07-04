938c9cba3c92ecee4b0b06f1664be29b9f167960
Lee Haugen <haugenlee@leegenes.local>
2017-07-04 14:38:15 -0400

    add search bar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/SearchBar'
 
 class App extends React.Component {
+  handleTermChange(term) {
+    console.log(term);
+  }
     render() {
         return (
             <div className="greeting">
-                <p className="greeting-text">Hello World!</p>
+                <SearchBar onTermChange={this.handleTermChange} />
             </div>
         );
     }
