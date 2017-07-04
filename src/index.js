7d96c1f35f5a9433e675e06e8bf3b8b94d54ab11
Lee Haugen <haugenlee@leegenes.local>
2017-07-04 14:20:01 -0400

    create App as components parent

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class App extends React.Component {
+    render() {
+        return (
+            <div className="greeting">
+                <p className="greeting-text">Hello World!</p>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
